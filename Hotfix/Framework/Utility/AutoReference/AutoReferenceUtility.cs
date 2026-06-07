b57a43aa91b03d131f96ace99a96b15a01e404de
Lcc <Lcc@qq.com>
2026-06-07 11:20:48 +0800

    fix

--- Hotfix/Framework/Utility/AutoReference/AutoReferenceUtility.cs
+++ Hotfix/Framework/Utility/AutoReference/AutoReferenceUtility.cs
@@ -1,18 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
-using UnityEngine;
+using Godot;
 
 namespace LccHotfix
 {
     public static class AutoReferenceUtility
     {
         #region 自动索引
-        public static void AutoReference(object obj, Transform transform)
+
+        public static void AutoReference(object obj, Node node)
         {
             Dictionary<string, FieldInfo> fieldInfoDict = new Dictionary<string, FieldInfo>();
             FieldInfo[] fieldInfos = obj.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-            Type objectType = typeof(UnityEngine.Object);
+            Type objectType = typeof(GodotObject);
             foreach (FieldInfo item in fieldInfos)
             {
                 if (item.FieldType.IsSubclassOf(objectType))
@@ -23,55 +24,50 @@ namespace LccHotfix
 
             if (fieldInfoDict.Count > 0)
             {
-                AutoReference(obj, transform, fieldInfoDict);
+                AutoReference(obj, node, fieldInfoDict);
             }
         }
-        public static void AutoReference(object obj, Transform transform, Dictionary<string, FieldInfo> fieldInfoDict)
+
+        public static void AutoReference(object obj, Node node, Dictionary<string, FieldInfo> fieldInfoDict)
         {
-            string name = transform.name.ToLower();
+            string name = node.Name.ToString().ToLower();
             if (fieldInfoDict.ContainsKey(name))
             {
-                if (fieldInfoDict[name].FieldType.Equals(typeof(GameObject)))
+                if (fieldInfoDict[name].FieldType.IsInstanceOfType(node))
                 {
-                    fieldInfoDict[name].SetValue(obj, transform.gameObject);
-                }
-                else if (fieldInfoDict[name].FieldType.Equals(typeof(Transform)))
-                {
-                    fieldInfoDict[name].SetValue(obj, transform);
-                }
-                else
-                {
-                    fieldInfoDict[name].SetValue(obj, transform.GetComponent(fieldInfoDict[name].FieldType));
+                    fieldInfoDict[name].SetValue(obj, node);
                 }
             }
 
-
-            Transform[] childrens = transform.GetComponentsInChildren<Transform>(true);
-
-            foreach (Transform item in childrens)
+            foreach (Node item in GetChildren(node))
             {
-                string itemName = item.name.ToLower();
+                string itemName = item.Name.ToString().ToLower();
                 if (fieldInfoDict.ContainsKey(itemName))
                 {
-                    if (fieldInfoDict[itemName].FieldType.Equals(typeof(GameObject)))
-                    {
-                        fieldInfoDict[itemName].SetValue(obj, item.gameObject);
-                    }
-                    else if (fieldInfoDict[itemName].FieldType.Equals(typeof(Transform)))
+                    if (fieldInfoDict[itemName].FieldType.IsInstanceOfType(item))
                     {
                         fieldInfoDict[itemName].SetValue(obj, item);
                     }
-                    else
-                    {
-                        fieldInfoDict[itemName].SetValue(obj, item.GetComponent(fieldInfoDict[itemName].FieldType));
-                    }
                 }
             }
         }
-        public static void AutoReference(object obj, GameObject gameObject)
+
+        private static List<Node> GetChildren(Node node)
         {
-            AutoReference(obj, gameObject.transform);
+            List<Node> result = new List<Node>();
+            CollectChildren(node, result);
+            return result;
         }
+
+        private static void CollectChildren(Node node, List<Node> list)
+        {
+            foreach (Node child in node.GetChildren())
+            {
+                list.Add(child);
+                CollectChildren(child, list);
+            }
+        }
+
         #endregion
     }
 }
