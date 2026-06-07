02e1d4e4ea6af5b348a6dec7ec0cba1affdd6dbf
Lcc <Lcc@qq.com>
2026-06-07 10:43:29 +0800

    fix

--- Framework/Launch/Launcher.cs
+++ Framework/Launch/Launcher.cs
@@ -3,33 +3,33 @@ using LccModel;
 
 public partial class Launcher : SingletonNode<Launcher>
 {
-    public override async void _Ready()
-    {
-        base._Ready();
+	public override async void _Ready()
+	{
+		base._Ready();
 
-        if (Engine.GetMainLoop() is not SceneTree tree)
-        {
-            return;
-        }
+		if (Engine.GetMainLoop() is not SceneTree tree)
+		{
+			return;
+		}
 
-        await tree.ToSignal(tree, SceneTree.SignalName.ProcessFrame);
-        StartLaunch();
-    }
+		await tree.ToSignal(tree, SceneTree.SignalName.ProcessFrame);
+		StartLaunch();
+	}
 
-    public void StartLaunch()
-    {
-        GD.Print("[Launch] Start");
+	public void StartLaunch()
+	{
+		GD.Print("[Launch] Start");
 
-        var operation = new LauncherOperation();
-        operation.Start();
+		var operation = new LauncherOperation();
+		operation.Start();
 
-        if (operation.Status == LauncherOperationStatus.Succeed)
-        {
-            GD.Print("[Launch] launcher succeed");
-        }
-        else
-        {
-            GD.PrintErr($"[Launch] launcher error : {operation.Error}");
-        }
-    }
+		if (operation.Status == LauncherOperationStatus.Succeed)
+		{
+			GD.Print("[Launch] launcher succeed");
+		}
+		else
+		{
+			GD.PrintErr($"[Launch] launcher error : {operation.Error}");
+		}
+	}
 }
